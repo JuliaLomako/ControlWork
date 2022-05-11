67d12fb60faf8a7e6ccb8b21ea7286f6227e95d6
Julia <jlomako@yandex.ru>
2022-05-11 18:43:33 +0300

    код написан

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -1,3 +1,30 @@
-﻿// КОНТРОЛЬНАЯ ЗАДАЧА: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
-// длина которых меньше либо равна 3 символов Первоначальный массив можно ввести  с клавиатуры, 
-// либо задать на старте выполнения алгоритма 
+﻿void MyNewArray(string[] inputArray, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < 11; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            newArray[count] = inputArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] inputArray = new string[11]
+{"hallo", "2", "world", ":-)", "1234", "1567", "-2",
+"computer science", "Rus", "Denmark", "Kazan"};
+string[] newArray = new string[11];
+
+MyNewArray(inputArray, newArray);
+PrintArray(newArray);
+
+
